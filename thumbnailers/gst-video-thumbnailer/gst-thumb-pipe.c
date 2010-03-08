228a24446bfe22d6e3915da857051930b13b380d
marcoil <marcoil@collabora.co.uk>
2010-03-08 15:23:48 +0100

    Remove another git_bus_poll, as Mikael Ottela advised.

--- thumbnailers/gst-video-thumbnailer/gst-thumb-pipe.c
+++ thumbnailers/gst-video-thumbnailer/gst-thumb-pipe.c
@@ -696,11 +696,7 @@ wait_for_image_buffer (ThumberPipe *pipe,
                GstMessage *message;
                GstElement *src;
 		
-		/* Yes, gst_bus_poll is not recommended and evil. We need it
-		   to be responsive to pre-unmount
-		*/
-
-	       message = gst_bus_poll (bus, GST_MESSAGE_ANY, timeout);
+	       message = gst_bus_timed_pop (bus, timeout);
 
                if (!message) {
                        g_set_error (error,
