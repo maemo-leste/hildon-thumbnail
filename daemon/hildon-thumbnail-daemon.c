d4b7ea682f07050488afcf2ca4b7c9c6ae5e4367
Merlijn Wajer <merlijn@wizzup.org>
2018-11-17 12:12:56 +0100

    hildon-thumbnail-daemon: add aarch64 support

--- daemon/hildon-thumbnail-daemon.c
+++ daemon/hildon-thumbnail-daemon.c
@@ -117,6 +117,9 @@ static gboolean do_shut_down_next_time = TRUE;
 #elif defined(__arm__)
 #define __NR_ioprio_set		314
 #define __NR_ioprio_get		315
+#elif defined(__aarch64__)
+#define __NR_ioprio_set		30
+#define __NR_ioprio_get		31
 #else
 #error "Unsupported architecture!"
 #endif
